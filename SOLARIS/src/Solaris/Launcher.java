2f32caa29011c9845ec49ca0b88db2c4bc9c72c1
WildCerebrus <mathieu.althuser@gmail.com>
2017-01-24 22:38:02 +0100

    init SolarSystem

--- SOLARIS/src/Solaris/Launcher.java
+++ SOLARIS/src/Solaris/Launcher.java
@@ -6,9 +6,11 @@ public class Launcher {
 	 * @author althuser
 	 * @param args Parameters of the launcher
 	 */
+	static int size = 1000;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		new SolarSystem(size,size);
 	}
 
 }
